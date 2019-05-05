87a6bcb3ad25ffe8701dfe08fffa1513d8b2a89c
Hassnian <hassnianxd@gmail.com>
2019-05-05 18:00:40 +0200

    Updates

---
+++ precache-manifest.fec6b7116e23a23e4788ffc7a992deb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4803fff7a346a4d9e6f",
+    "url": "/BurgerBuilder/static/js/runtime~main.70232762.js"
+  },
+  {
+    "revision": "24f76afa29a7fe5b358d",
+    "url": "/BurgerBuilder/static/js/main.3f59bffa.chunk.js"
+  },
+  {
+    "revision": "66f395d302fbe0e167c4",
+    "url": "/BurgerBuilder/static/js/2.b6fe4141.chunk.js"
+  },
+  {
+    "revision": "24f76afa29a7fe5b358d",
+    "url": "/BurgerBuilder/static/css/main.98edbe07.chunk.css"
+  },
+  {
+    "revision": "835cab40e970a94d32ef920c1548b2af",
+    "url": "/BurgerBuilder/index.html"
+  }
+];
